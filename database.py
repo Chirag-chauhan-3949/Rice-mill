7fce06802b112dc57fb09ea0923157e6d884ec70
Chirag-chauhan-3949 <chiragsinghchauhan3949323@gmail.com>
2023-12-14 15:15:04 +0530

    update

--- database.py
+++ database.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-URL_DATABASE = "mysql+pymysql://root:MyN3wP4ssw0rd@localhost:3306/ricemill"
+# URL_DATABASE = "mysql+pymysql://root:MyN3wP4ssw0rd@localhost:3306/ricemill"
 # URL_DATABASE = "mysql+pymysql://root:MyN3wP4ssw0rd@localhost:3306/ricemill2"
 # URL_DATABASE = "mysql+pymysql://root:MyN3wP4ssw0rd@localhost:3306/testing_rice_mill"
 # URL_DATABASE = "mysql+pymysql://root:MyN3wP4ssw0rd@localhost:3306/testing"
 # URL_DATABASE = "mysql+pymysql://root:Chirag3949@localhost:3306/ricemill"
+URL_DATABASE = "mysql+pymysql://chirag:zavr65@localhost:3306/ricemill"
 
 
 engine = create_engine(URL_DATABASE)
